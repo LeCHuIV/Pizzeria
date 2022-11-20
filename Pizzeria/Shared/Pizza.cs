f7733fc4e0946099444dc60644e75108ef8c3c05
Patryk Lech <patryk.lech@altone.pl>
2022-11-20 17:43:08 +0100

    Dodanie Menu w wersji beta

---
+++ Pizzeria/Shared/Pizza.cs
@@ -0,0 +1,22 @@
+﻿namespace Pizzeria.Shared
+{
+    public class Pizza
+    {
+        public int ID { get; set; }
+        public string Nazwa { get; set; }
+        public decimal Cena { get; set; }
+        public string Opis { get; set; }
+        public string Img { get; set; }
+        public static Pizza FromCsv(string csvLine)
+        {
+            string[] values = csvLine.Split(';');
+            Pizza pizza = new Pizza();
+            pizza.ID = Convert.ToInt16(values[0]);
+            pizza.Nazwa = Convert.ToString(values[1]);
+            pizza.Cena = Convert.ToDecimal(values[2]);
+            pizza.Opis = Convert.ToString(values[3]);
+            pizza.Img = Convert.ToString(values[4]);
+            return pizza;
+        }
+    }
+}
